4723f53b2a7a935d28cbc53f6448d6569487d3ed
tkieras42 <tkieras42@gmail.com>
2023-07-06 14:51:15 -0400

    added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,41 @@ $(function () {
       hoursContainer.children().eq(i).addClass("present");
     }
   }
+
+  function saveWords(event){
+    
+    var textE1 = $(event.currentTarget);
+    var text = textE1.value;
+     if(textE1.is('#hour-9')){
+      localStorage.setItem("hour-9", text);
+     }  
+     else if(textE1.is('#hour-10')){
+      localStorage.setItem("hour-10", text);
+     }
+     else if(textE1.is('#hour-11')){
+      localStorage.setItem("hour-11", text);
+     }
+     else if(textE1.is('#hour-12')){
+      localStorage.setItem("hour-12", text);
+     }
+     else if(textE1.is('#hour-13')){
+      localStorage.setItem("hour-13", text);
+     }
+     else if(textE1.is('#hour-14')){
+      localStorage.setItem("hour-14", text);
+     }
+     else if(textE1.is('#hour-15')){
+      localStorage.setItem("hour-15", text);
+     }
+     else if(textE1.is('#hour-16')){
+      localStorage.setItem("hour-16", text);
+     }
+     else if(textE1.is('#hour-17')){
+      localStorage.setItem("hour-17", text);
+     }
+    
+     
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -42,7 +77,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  setDate(now);
-  
+  hoursContainer.on('click', '.btn', saveWords);
+  setDate(now);  
   whatColor();
 });
